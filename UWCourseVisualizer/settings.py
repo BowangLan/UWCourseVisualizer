ef8fa440651618d4099dd08c075f82c25cb69571
BowangLan <60339191+BowangLan@users.noreply.github.com>
2020-11-30 16:49:08 -0800

    Git demo

--- UWCourseVisualizer/settings.py
+++ UWCourseVisualizer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1a%5gy%x$7h76@%84jj)-xe3)uc@8yc8-49s%$vgv+w3+fzy(r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "course_search",
 ]
 
 MIDDLEWARE = [
